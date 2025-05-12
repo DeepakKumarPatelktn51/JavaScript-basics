61a1bb842f5964aae92d9ec17fc761101a32eb6c
DeepakKumarPatelktn51 <deepakktn51@gmail.com>
2025-05-12 06:13:00 +0000

    comparison of datatypes done

---
+++ js_basics/04_comparison_of_datatypes.js
@@ -0,0 +1,25 @@
+// console.log( 2>0)
+// console.log(2>=1)
+// console.log(2==1)
+// console.log(4<2)
+
+//console.log("2" > 1)
+//console.log("02"  > 1)
+
+// console.log(null >0)
+// console.log(null == 0)
+// console.log(null <= 0)
+// console.log(null >=0)
+
+// console.log(undefined >0)
+// console.log(undefined == 0)
+// console.log(undefined <= 0)
+// console.log(undefined >=0)
+
+
+// Note : avoid null and undefined comparison
+
+// 3rd comparison is strictly check   symbol ===  triple equalto checks for the number and its data-types both
+console.log("3" === 3)
+console.log("3" == 3)
+
