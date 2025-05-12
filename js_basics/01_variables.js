d1310d510aba7c1326b59dd0ad28ebbfe1c1aac8
DeepakKumarPatelktn51 <deepakktn51@gmail.com>
2025-05-12 04:35:10 +0000

    variables , datatypes done

---
+++ js_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=1345345
+let accountEmail="deepakktn515398@gmail.com"
+var accountPassword="Abdcfh@53hf75"
+accountCity="Jaipur"
+
+accountEmail="dkp@gmisal.vom"
+accountPassword="4343434"
+accountCity="Hyderabad"
+
+
+/*
+prefer not to use " var " variables 
+because of issue in block scope and functional scope
+
+*/
+console.table([accountEmail,accountId,accountPassword,accountCity])
---
+++ js_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict";   // treat all js code as newer version
+
+// alert(3+5)  // we are using nodejs , not browser
+
+                            // primitive datatypes
+let name="Deepak"          // string -> " "  oe ' '
+let age=25                 // number  
+let isLoggedIn=true        //boolean
+let sbs;                   //output : undefined means value not assigned
+let data=null;             //output : null
+console.log(data)
+
+                           //reference datatypes
+                           //object
+console.log(typeof("deepak"))   
+console.log(typeof(undefined))
+console.log(typeof(null))
