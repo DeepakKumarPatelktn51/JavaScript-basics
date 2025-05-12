151f037a65f36f8d2742f49e998634e4eed5ce64
DeepakKumarPatelktn51 <deepakktn51@gmail.com>
2025-05-12 05:56:55 +0000

    conversion of datatypes , prifix, postfix, operations done

---
+++ js_basics/03_datatypeConversion.js
@@ -0,0 +1,61 @@
+ let score =33
+// console.log(typeof score)
+// console.log(typeof (score))
+
+ let score1="33"
+// console.log(typeof score1)
+// console.log(typeof (score1))
+
+ let stringInNumber=Number(score1)
+// console.log(stringInNumber)
+// console.log(typeof stringInNumber)
+
+let name="Deepak"
+let ConvName=Number(name)
+//console.log(ConvName)
+//console.log(typeof name)
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+
+let numb=25;
+let stringnumber1=String(numb)
+//console.log(stringnumber1)
+//console.log(typeof stringnumber1)
+
+// "33"  -- > 33
+//  "33nakdn"  --->NaN
+//   true  ------>1
+//false  -------> 0
+// 1-->true ,   0---> false
+// ""  -->false
+// "deepak"  --->true
+
+
+//-----------------operations-----------------------------//
+
+// console.log("1"+2)
+// console.log(4+"3")
+// console.log(4+4+"5")
+// console.log("5"+ 6+7)
+//console.log(true)
+
+// --------------prifix and postfix---------------------//
+let a=5
+let b=a++
+//console.log(b)
+
+let a1=6;
+let b1=++a1
+//console.log(b1)
+
+let a2=8;
+let b2=--a2
+//console.log(b2 +" "+a2)
+
+let g1=45;
+console.log("g1 : "+g1)
+let g2=g1--;
+console.log("g1 : "+g1 +" "+ "g2 : "+ g2)
