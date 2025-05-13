4cf3882727b7439b0d306522ccfc82e9588aedf0
DeepakKumarPatelktn51 <deepakktn51@gmail.com>
2025-05-13 03:39:39 +0000

    number,maths,dates in JS done

---
+++ js_basics/07_numbers&Maths.js
@@ -0,0 +1,27 @@
+const num=100
+//console.log(num)
+
+const num1=new Number(50)
+//console.log(num1)
+
+//console.log(num1.toString())
+//console.log(num1.toString().length)
+//console.log(num1.toFixed(2))
+
+
+const num2=1000000
+//console.log(num2.toLocaleString('en-IN'))
+//console.log(num2.toLocaleString('en-US'))
+
+
+
+//------------------------------------maths--------------------------------------------------//
+// console.log(Math)
+// console.log(Math.abs(-5))
+// console.log(Math.round(6.45))
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(4.3))
+// console.log(Math.sqrt(25))
+// console.log(Math.min(3,5,6,7,1))
+// console.log(Math.max(45,45,7787,9,7))
+console.log(Math.random())
---
+++ js_basics/08_Dates.js
@@ -0,0 +1,14 @@
+let myDate=new Date();
+// console.log(myDate)
+
+// console.log(myDate.toString())
+// console.log(myDate.toLocaleString('en-IN'))
+
+// console.log(typeof myDate)
+
+let myCreatedDate=new Date(2025, 5, 23)
+//console.log(myCreatedDate.toDateString())
+
+let myCreatedDate1=new Date(2025,0,10, 5,3)
+//console.log(myCreatedDate1.toLocaleDateString())
+console.log(myCreatedDate1.getMonth())
